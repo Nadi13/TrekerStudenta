346a42611b4c340c6791f3e57c43e3980b817ba7
Nadi13 <nadia_yudintseva@mail.ru>
2022-12-08 22:44:35 +0600

    new

--- app/src/main/java/com/example/treker/Activity_Page_2.java
+++ app/src/main/java/com/example/treker/Activity_Page_2.java
@@ -1,6 +1,6 @@
 package com.example.treker;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Activity_Page_2 extends AppCompatActivity {
